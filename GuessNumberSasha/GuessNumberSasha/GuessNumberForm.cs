144b967e60f9e5e75f56831be0d3db15a9d337c5
Sasha Malko <36854156+SashaMalko0810@users.noreply.github.com>
2019-10-03 12:32:09 -0700

    Made changes

--- GuessNumberSasha/GuessNumberSasha/GuessNumberForm.cs
+++ GuessNumberSasha/GuessNumberSasha/GuessNumberForm.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Sasha Malko 
+ * Created on: 1-Oct-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #17 - Random Number
+ * This program allows the user to play a guess my number game. 
+ */
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,24 +23,32 @@ namespace GuessNumberSasha
         public frmGuessNumber()
         {
             InitializeComponent();
+
+            //Hide the answer labels
             this.lblAnswer.Hide();
             this.picAnswer.Hide();
         }
 
         private void BtnCheck_Click(object sender, EventArgs e)
         {
+            //declare contstants
             const int MIN_NUM = 1;
             const int MAX_NUM = 10;
 
+            //declare local variables
             int guess, random;
 
+            //generate a random number
             Random rnd = new Random();
             random = rnd.Next(MIN_NUM, MAX_NUM);
 
+            //convert the string into an integer
             guess = int.Parse(txtGuess.Text);
+
 
             if (guess == random)
             {
+                //display that the answer is correct
                 lblAnswer.Text = "You guessed correctly!";
                 this.lblAnswer.Show();
                 picAnswer.Image = Properties.Resources.checkmark;
@@ -41,6 +56,7 @@ namespace GuessNumberSasha
             }
             else
             {
+                //display that the answer is incorrect
                 lblAnswer.Text = "You guessed incorrectly!";
                 this.lblAnswer.Show();
                 picAnswer.Image = Properties.Resources.red_x;
