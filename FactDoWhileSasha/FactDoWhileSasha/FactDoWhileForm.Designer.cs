4699ed7c0901dc200275bac5153a64622743b25b
Sasha Malko <36854156+SashaMalko0810@users.noreply.github.com>
2019-10-08 16:48:59 -0700

    Worked on factorial do while

---
+++ FactDoWhileSasha/FactDoWhileSasha/FactDoWhileForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace FactDoWhileSasha
+{
+    partial class frmFactDoWhile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblValue = new System.Windows.Forms.Label();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblValue
+            // 
+            this.lblValue.AutoSize = true;
+            this.lblValue.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValue.ForeColor = System.Drawing.Color.White;
+            this.lblValue.Location = new System.Drawing.Point(59, 56);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(672, 30);
+            this.lblValue.TabIndex = 0;
+            this.lblValue.Text = "What number would you like to perform a factorial calculation on?";
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(328, 141);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(100, 20);
+            this.txtValue.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(319, 216);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(118, 33);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold);
+            this.lblAnswer.ForeColor = System.Drawing.Color.White;
+            this.lblAnswer.Location = new System.Drawing.Point(335, 331);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 30);
+            this.lblAnswer.TabIndex = 3;
+            // 
+            // frmFactDoWhile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.lblValue);
+            this.Name = "frmFactDoWhile";
+            this.Text = "Factorial Do While by Sasha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblValue;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ FactDoWhileSasha/FactDoWhileSasha/FactDoWhileForm.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Created by: Sasha Malko
+ * Created on: 8-Oct-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #20 - Factorial Do While
+ * This program gives the user the answer to the factorial problem they have entered.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FactDoWhileSasha
+{
+    public partial class frmFactDoWhile : Form
+    {
+        public frmFactDoWhile()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            double fact = 1;
+            double factTotal = 1;
+            double uValue; 
+
+            // convert the string from the text box to a double
+            uValue = double.Parse(txtValue.Text);
+
+            // perform the factorial calculation until the factorial value equals the number the user entered
+            do
+            {
+                // multiply the total by the factorial variable 
+                factTotal = factTotal * fact;
+                // increase the factoriale variable by 1 each time 
+                fact = fact + 1;
+
+            } while (fact <= uValue);
+
+            // display the user's value with a factorial sign and the total with its respective answer 
+            this.lblAnswer.Text = this.txtValue.Text + "! = " + Convert.ToString(factTotal); 
+        }
+    }
+}
