56dd37c4015ad73f802f3dd3b7a2a56653d44194
Sasha Malko <sasha.malko@ocsbstudent.ca>
2019-10-09 12:55:45 -0400

    Worked on walking man

---
+++ WalkingManSasha/WalkingManSasha/WalkingManForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WalkingManSasha
+{
+    partial class frmWalkingMan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWalk = new System.Windows.Forms.Button();
+            this.picWalkingMan = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picWalkingMan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnWalk
+            // 
+            this.btnWalk.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWalk.Location = new System.Drawing.Point(337, 365);
+            this.btnWalk.Name = "btnWalk";
+            this.btnWalk.Size = new System.Drawing.Size(88, 33);
+            this.btnWalk.TabIndex = 0;
+            this.btnWalk.Text = "Walk";
+            this.btnWalk.UseVisualStyleBackColor = true;
+            this.btnWalk.Click += new System.EventHandler(this.BtnWalk_Click);
+            // 
+            // picWalkingMan
+            // 
+            this.picWalkingMan.Image = global::WalkingManSasha.Properties.Resources.walk1;
+            this.picWalkingMan.Location = new System.Drawing.Point(292, 99);
+            this.picWalkingMan.Name = "picWalkingMan";
+            this.picWalkingMan.Size = new System.Drawing.Size(180, 180);
+            this.picWalkingMan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picWalkingMan.TabIndex = 1;
+            this.picWalkingMan.TabStop = false;
+            // 
+            // frmWalkingMan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picWalkingMan);
+            this.Controls.Add(this.btnWalk);
+            this.Name = "frmWalkingMan";
+            this.Text = "Walking Man by Sasha";
+            ((System.ComponentModel.ISupportInitialize)(this.picWalkingMan)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWalk;
+        private System.Windows.Forms.PictureBox picWalkingMan;
+    }
+}
+
---
+++ WalkingManSasha/WalkingManSasha/WalkingManForm.cs
@@ -0,0 +1,126 @@
+﻿/*
+ * Created by: Sasha Malko
+ * Created on: 9-Oct-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #21 - Walking Man
+ * This program allows for a cartoon man to start walking on the form once the walk button is clicked.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// add the following library to use the sleep timer
+using System.Threading; 
+
+namespace WalkingManSasha
+{
+    public partial class frmWalkingMan : Form
+    {
+        public frmWalkingMan()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnWalk_Click(object sender, EventArgs e)
+        {
+            // declare local variables and constants
+            const byte MAX_FRAMES = 10;
+            int pictureFrameCounter = 1;
+
+            // continue this loop while the frame counter has not reached the max number of frames we have to display
+            while (pictureFrameCounter < MAX_FRAMES + 1)
+            {
+                if (pictureFrameCounter == 1)
+                {
+                    // display an image
+                    this.picWalkingMan.Image = Properties.Resources.walk1;
+                    // add one to the picture frame counter
+                    pictureFrameCounter = pictureFrameCounter + 1;
+                }
+
+                else if (pictureFrameCounter == 2)
+                {
+                    // display an image
+                    this.picWalkingMan.Image = Properties.Resources.walk2;
+                    // add one to the picture frame counter
+                    pictureFrameCounter = pictureFrameCounter + 1;
+                }
+
+                else if (pictureFrameCounter == 3)
+                {
+                    // display an image
+                    this.picWalkingMan.Image = Properties.Resources.walk3;
+                    // add one to the picture frame counter
+                    pictureFrameCounter = pictureFrameCounter + 1;
+                }
+
+                else if (pictureFrameCounter == 4)
+                {
+                    // display an image
+                    this.picWalkingMan.Image = Properties.Resources.walk4;
+                    // add one to the picture frame counter
+                    pictureFrameCounter = pictureFrameCounter + 1;
+                }
+
+                else if (pictureFrameCounter == 5)
+                {
+                    // display an image
+                    this.picWalkingMan.Image = Properties.Resources.walk5;
+                    // add one to the picture frame counter
+                    pictureFrameCounter = pictureFrameCounter + 1;
+                }
+
+                else if (pictureFrameCounter == 6)
+                {
+                    // display an image
+                    this.picWalkingMan.Image = Properties.Resources.walk6;
+                    // add one to the picture frame counter
+                    pictureFrameCounter = pictureFrameCounter + 1;
+                }
+
+                else if (pictureFrameCounter == 7)
+                {
+                    // display an image
+                    this.picWalkingMan.Image = Properties.Resources.walk7;
+                    // add one to the picture frame counter
+                    pictureFrameCounter = pictureFrameCounter + 1;
+                }
+
+                else if (pictureFrameCounter == 8)
+                {
+                    // display an image
+                    this.picWalkingMan.Image = Properties.Resources.walk8;
+                    // add one to the picture frame counter
+                    pictureFrameCounter = pictureFrameCounter + 1;
+                }
+
+                else if (pictureFrameCounter == 9)
+                {
+                    // display an image
+                    this.picWalkingMan.Image = Properties.Resources.walk9;
+                    // add one to the picture frame counter
+                    pictureFrameCounter = pictureFrameCounter + 1;
+                }
+
+                else
+                {
+                    // display an image
+                    this.picWalkingMan.Image = Properties.Resources.walk10;
+                    // add one to the picture frame counter
+                    pictureFrameCounter = pictureFrameCounter + 1;
+                }
+
+                // refresh the form
+                this.Refresh();
+
+                // pause the loop for 100 milliseconds
+                Thread.Sleep(100); 
+            }
+        }
+    }
+}
