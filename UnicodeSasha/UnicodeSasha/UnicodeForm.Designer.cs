ebbbb55857d292e02254d05d49c5b3d5a968286b
Sasha Malko <sasha.malko@ocsbstudent.ca>
2019-10-21 12:39:57 -0400

    Worked on unicode

---
+++ UnicodeSasha/UnicodeSasha/UnicodeForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace UnicodeSasha
+{
+    partial class frmUnicode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstConversions = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(326, 89);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(104, 38);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // lstConversions
+            // 
+            this.lstConversions.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstConversions.FormattingEnabled = true;
+            this.lstConversions.ItemHeight = 24;
+            this.lstConversions.Location = new System.Drawing.Point(255, 200);
+            this.lstConversions.Name = "lstConversions";
+            this.lstConversions.Size = new System.Drawing.Size(243, 172);
+            this.lstConversions.TabIndex = 1;
+            // 
+            // frmUnicode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PapayaWhip;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstConversions);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmUnicode";
+            this.Text = "Unicode by Sasha";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstConversions;
+    }
+}
+
---
+++ UnicodeSasha/UnicodeSasha/UnicodeForm.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Created by: Sasha Malko
+ * Created on: 21-Oct-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #24 - Unicode
+ * This program converts each letter into the number that represents it using unicode.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnicodeSasha
+{
+    public partial class frmUnicode : Form
+    {
+        public frmUnicode()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            int counter = 65; 
+            string letter; 
+
+            // clear the items in the list box
+            this.lstConversions.Items.Clear();
+
+            // perform this loop until the counter is equal to 90
+            do
+            {
+                // convert the counter value into the letter that it represents
+                letter = Char.ConvertFromUtf32(counter);
+
+                // display each letter and its number value in the list box
+                this.lstConversions.Items.Add(letter + " -> " + Convert.ToString(counter));
+
+                // increase the counter by 1 each time
+                counter = counter + 1;
+
+            } while (counter <= 90);
+        }
+    }
+}
