5c1e145eab6c2c68888356fbfd95f0301bf0ccaf
Sasha Malko <sasha.malko@ocsbstudent.ca>
2019-10-22 12:48:47 -0400

    Worked on control objects

---
+++ ControlObjectsSasha/ControlObjectsSasha/ControlObjectsForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ControlObjectsSasha
+{
+    partial class frmControlObjects
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblColour = new System.Windows.Forms.Label();
+            this.btnColour = new System.Windows.Forms.Button();
+            this.lstColour = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblColour
+            // 
+            this.lblColour.AutoSize = true;
+            this.lblColour.BackColor = System.Drawing.Color.Transparent;
+            this.lblColour.Font = new System.Drawing.Font("Franklin Gothic Medium", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblColour.Location = new System.Drawing.Point(219, 61);
+            this.lblColour.Name = "lblColour";
+            this.lblColour.Size = new System.Drawing.Size(316, 26);
+            this.lblColour.TabIndex = 0;
+            this.lblColour.Text = "Would you like to change colours?";
+            this.lblColour.Click += new System.EventHandler(this.LblColour_Click);
+            // 
+            // btnColour
+            // 
+            this.btnColour.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnColour.Location = new System.Drawing.Point(292, 124);
+            this.btnColour.Name = "btnColour";
+            this.btnColour.Size = new System.Drawing.Size(154, 42);
+            this.btnColour.TabIndex = 1;
+            this.btnColour.Text = "Change Colours";
+            this.btnColour.UseVisualStyleBackColor = true;
+            this.btnColour.Click += new System.EventHandler(this.BtnColour_Click);
+            // 
+            // lstColour
+            // 
+            this.lstColour.FormattingEnabled = true;
+            this.lstColour.Location = new System.Drawing.Point(247, 220);
+            this.lstColour.Name = "lstColour";
+            this.lstColour.Size = new System.Drawing.Size(249, 173);
+            this.lstColour.TabIndex = 2;
+            // 
+            // frmControlObjects
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstColour);
+            this.Controls.Add(this.btnColour);
+            this.Controls.Add(this.lblColour);
+            this.Name = "frmControlObjects";
+            this.Text = "For...Each with Control Objects by Sasha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblColour;
+        private System.Windows.Forms.Button btnColour;
+        private System.Windows.Forms.ListBox lstColour;
+    }
+}
+
---
+++ ControlObjectsSasha/ControlObjectsSasha/ControlObjectsForm.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * Created by: Sasha Malko
+ * Created on: 22-Oct-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #25 - Control Objects
+ * This program has multiple different Control Objects and changes some property for each type of control object when clicked.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlObjectsSasha
+{
+    public partial class frmControlObjects : Form
+    {
+        public frmControlObjects()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnColour_Click(object sender, EventArgs e)
+        {
+            // loop through each object on the form and make it green
+            foreach (Control aControlObject in this.Controls)
+            {
+                aControlObject.BackColor = Color.Green;
+
+                // if the object is a label, make it purple
+                if (aControlObject.GetType() == typeof(Label))
+                {
+                    aControlObject.BackColor = Color.Purple;
+                }
+
+                // if the object is a list box, make it red
+                if (aControlObject.GetType() == typeof(ListBox))
+                {
+                    aControlObject.BackColor = Color.Red;
+                }
+            }
+        }
+
+        private void LblColour_Click(object sender, EventArgs e)
+        {
+            // loop through each object on the form and make it yellow
+            foreach (Control aControlObject in this.Controls)
+            {
+                aControlObject.BackColor = Color.Yellow;
+
+                // if the object is a label, make it orange
+                if (aControlObject.GetType() == typeof(Button))
+                {
+                    aControlObject.BackColor = Color.Orange;
+                }
+
+                // if the object is a list box, make it blue
+                if (aControlObject.GetType() == typeof(ListBox))
+                {
+                    aControlObject.BackColor = Color.Blue;
+                }
+            }
+        }
+    }
+}
