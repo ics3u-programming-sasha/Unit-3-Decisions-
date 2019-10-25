f2e22a6b2b073ca54bce1ca9fad6130433367527
Sasha Malko <sasha.malko@ocsbstudent.ca>
2019-10-25 13:10:26 -0400

    Made changes

---
+++ RunningAverageSasha/RunningAverageSasha/RunningAverageForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace RunningAverageSasha
+{
+    partial class frmRunningAverage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEnter = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.lblExit = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEnter
+            // 
+            this.lblEnter.AutoSize = true;
+            this.lblEnter.Font = new System.Drawing.Font("Adobe Devanagari", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnter.Location = new System.Drawing.Point(125, 101);
+            this.lblEnter.Name = "lblEnter";
+            this.lblEnter.Size = new System.Drawing.Size(250, 28);
+            this.lblEnter.TabIndex = 0;
+            this.lblEnter.Text = "Enter a number from 0 to 100:";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(480, 106);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber.TabIndex = 1;
+            // 
+            // lblExit
+            // 
+            this.lblExit.AutoSize = true;
+            this.lblExit.Font = new System.Drawing.Font("Adobe Devanagari", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExit.Location = new System.Drawing.Point(125, 129);
+            this.lblExit.Name = "lblExit";
+            this.lblExit.Size = new System.Drawing.Size(133, 26);
+            this.lblExit.TabIndex = 2;
+            this.lblExit.Text = "(Enter -1 to exit.)";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(277, 205);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(179, 47);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate Average";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.Location = new System.Drawing.Point(223, 347);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(24, 25);
+            this.lblAverage.TabIndex = 4;
+            this.lblAverage.Text = "?";
+            // 
+            // frmRunningAverage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblExit);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.lblEnter);
+            this.Name = "frmRunningAverage";
+            this.Text = "Running Average by Sasha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEnter;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label lblExit;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblAverage;
+    }
+}
+
---
+++ RunningAverageSasha/RunningAverageSasha/RunningAverageForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RunningAverageSasha
+{
+    public partial class frmRunningAverage : Form
+    {
+        double value = 0, numberOfValues = 0;
+
+        public frmRunningAverage()
+        {
+            InitializeComponent();
+        }
+
+       private void BtnCalculate_Click(object sender, EventArgs e)
+       {
+            double average, sum = 0 ; 
+
+            sum = double.Parse(this.txtNumber.Text);
+
+            numberOfValues++;
+
+            sum = value + sum; 
+
+            average = sum / numberOfValues;
+
+            lblAverage.Text = Convert.ToString(average);
+       }
+    }
+}
